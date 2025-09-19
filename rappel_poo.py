8145e98f25d0883b16af15acff1cd93a3fa078b3
Lockman <lbensalah@spidr.fr>
2025-09-19 15:12:30 +0200

    couche database is ok

---
+++ rappel_poo.py
@@ -0,0 +1,53 @@
+###### Classes ######
+
+#%%
+class Chien:
+    pass
+
+
+###### Objet (Instance) ######
+
+#%%
+mon_chien = Chien()
+print(type(mon_chien))
+# %%
+
+
+
+
+####### Attributs ######
+
+#%%
+class Chien:
+    def __init__(self, nom, race):
+        self.nom = nom
+        self.race = race
+
+
+#%%
+mon_chien = Chien("Pipo", "Labrador")
+print(mon_chien.nom)
+print(mon_chien.race)
+#%%
+
+
+
+####### Méthodes ######
+
+#%%
+class Chien:
+    def __init__(self, nom, race):
+        self.nom = nom
+        self.race = race
+    
+    def aboyer(self):
+        print(f"{self.nom} aboie !")
+#%%        
+        
+rex = Chien("Rex", "Berger Allemand")
+print(rex.aboyer())
+
+
+
+
+# %%
